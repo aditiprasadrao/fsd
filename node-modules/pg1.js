131d4d3fb6ea8b5432bd00d9f8b7f523828c1845
aditi <aditiprao1@gmail.com>
2025-05-27 23:24:50 +0530

    Add pg1.js

---
+++ node-modules/pg1.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+
+alert("The sum of two numbers is: " + sum);
+
+let cities = ["New York", "Los Angeles", "Chicago", "Paris", "Sydney"];
+console.log("Length of cities: " + cities.length);
+
+cities.push("Tokyo");
+console.log("Cities after adding Tokyo: " + cities);
+
+cities.shift();
+console.log("Cities after removing the first city: " + cities);
+
+let specific_city = cities.indexOf("Chicago");
+console.log("Index of a specific city: " + specific_city);
+
